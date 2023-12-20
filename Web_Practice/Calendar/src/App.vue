adf1a69aa629680dfb143dccba2f3426460971d8
suehwanBoo <rndjdieo119@naver.com>
2023-12-20 13:55:21 +0900

    web-calendar complete

---
+++ Web_Practice/Calendar/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <HomeView/>
+  </div>
+</template>
+
+<script setup>
+import HomeView from './views/HomeView.vue';
+
+</script>
+
+<style  scoped>
+
+</style>
